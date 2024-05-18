cc0ee9469c4f7eaae38f2d71fcd45170cf5db989
zhang7391 <zhang7391@protonmail.com>
2024-05-19 02:45:48 +0800

    add a description for the exceptions.

---
+++ src/main/java/taiwan/PokerTestData.java
@@ -0,0 +1,10 @@
+package taiwan;
+
+
+public class PokerTestData
+{
+	protected PokerTestData() {}
+
+	public static final int INT_DATA_NUMBER = 625;
+	public static final int BYTE_DATA_NUMBER = 2500;
+}
--- src/test/java/taiwan/Test_PokerTest.java
+++ src/test/java/taiwan/Test_PokerTest.java
@@ -4,15 +4,13 @@ import taiwan.PokerTest;
 import org.junit.jupiter.api.Test;
 import java.security.SecureRandom;
 
+import static taiwan.PokerTestData.INT_DATA_NUMBER;
+import static taiwan.PokerTestData.BYTE_DATA_NUMBER;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class Test_PokerTest
 {
-	private final int DOUBLE_LENGTH = 8;
-	private final int INT_DATA_NUMBER = 625;
-	private final int BYTE_DATA_NUMBER = 2500;
-
 	private final PokerTest pokerTest = new PokerTest();
 	private final SecureRandom random = new SecureRandom();
 
@@ -20,24 +18,26 @@ public class Test_PokerTest
 	@Test
 	public void byteRNG_Checker()
 	{
-		byte[] data = new byte[BYTE_DATA_NUMBER];
+		final byte[] data = new byte[BYTE_DATA_NUMBER];
+		final String exceptionMessage = "Byte RNG not passed.";
 
 		for(int x=0; 2000>x; x++)
 		{
 			random.nextBytes(data);
-			assertTrue(pokerTest.run(data));
+			assertTrue(pokerTest.run(data), "Byte RNG not passed.");
 		}
 	}
 
 	@Test
 	public void intRNG_Checker()
 	{
-		int[] data = new int[INT_DATA_NUMBER];
+		final int[] data = new int[INT_DATA_NUMBER];
+		final String exceptionMessage = "Integer RNG not passed.";
 
 		for(int x=0; 2000>x; x++)
 		{
 			for(int y=0; INT_DATA_NUMBER>y; y++) data[y] = random.nextInt();
-			assertTrue(pokerTest.run(data));
+			assertTrue(pokerTest.run(data), exceptionMessage);
 		}
 	}
 }
