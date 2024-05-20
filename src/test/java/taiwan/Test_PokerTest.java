f2a89924c9af3709f62f5094bf9bdf116c0dbe52
zhang7391 <zhang7391@protonmail.com>
2024-05-20 13:01:15 +0800

    revise the testing standards.

--- src/test/java/taiwan/Test_PokerTest.java
+++ src/test/java/taiwan/Test_PokerTest.java
@@ -1,12 +1,15 @@
 package taiwan;
 
 import taiwan.PokerTest;
+import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
 import java.security.SecureRandom;
 
+import static java.math.RoundingMode.HALF_UP;
+import static java.math.MathContext.UNLIMITED;
 import static taiwan.PokerTestData.INT_DATA_NUMBER;
 import static taiwan.PokerTestData.BYTE_DATA_NUMBER;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 
 public class Test_PokerTest
@@ -23,11 +26,17 @@ public class Test_PokerTest
 		final byte[] data = new byte[BYTE_DATA_NUMBER];
 		final String exceptionMessage = "Byte RNG not passed.";
 
+		BigDecimal failCounter = BigDecimal.ZERO;
+
 		for(int x=0; TESTCASE_NUMBER>x; x++)
 		{
 			random.nextBytes(data);
-			assertTrue(pokerTest.run(data), "Byte RNG not passed.");
+			if(!pokerTest.run(data)) failCounter.add(BigDecimal.ONE, UNLIMITED);
 		}
+
+		// Accuracy must be above 99.9%.
+		failCounter = failCounter.divide(new BigDecimal(TESTCASE_NUMBER, UNLIMITED), 10, HALF_UP);
+		if(failCounter.compareTo(new BigDecimal("0.001", UNLIMITED)) == 1) fail("Accuracy must be above 99.9%.");
 	}
 
 	@Test
@@ -36,10 +45,16 @@ public class Test_PokerTest
 		final int[] data = new int[INT_DATA_NUMBER];
 		final String exceptionMessage = "Integer RNG not passed.";
 
+		BigDecimal failCounter = BigDecimal.ZERO;
+
 		for(int x=0; TESTCASE_NUMBER>x; x++)
 		{
 			for(int y=0; INT_DATA_NUMBER>y; y++) data[y] = random.nextInt();
-			assertTrue(pokerTest.run(data), exceptionMessage);
+			if(!pokerTest.run(data)) failCounter.add(BigDecimal.ONE, UNLIMITED);
 		}
+
+		// Accuracy must be above 99.9%.
+		failCounter = failCounter.divide(new BigDecimal(TESTCASE_NUMBER, UNLIMITED), 10, HALF_UP);
+		if(failCounter.compareTo(new BigDecimal("0.001", UNLIMITED)) == 1) fail("Accuracy must be above 99.9%.");
 	}
 }
